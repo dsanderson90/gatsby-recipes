dc5b4fe0cb86104cc9a465cbe8c377423bbf5c78
David <sanderdj90@gmail.com>
2020-06-08 20:12:03 -0600

    feat: add mdx, emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -28,5 +28,27 @@ module.exports = {
       },
     },
     "gatsby-plugin-postcss",
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "MDXImages",
+        path: "src/images/",
+      },
+      __key: "src/images",
+    },
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
+    <SEO title="Gatsby Recipes" />
     <p>Gatsby Recipes.</p>
-    
+    <Link to="/usingemotion/">using Emotion</Link> <br />
     <Link to="/usingtailwind/">using Tailwind</Link> <br />
     <Link to="/using-typescript/">Using TypeScript</Link>
   </Layout>
